c702d478308c29ff8f888475290e143df862fc6f
Alexsandro souza <alexsandrosouzasilva68@gmail.com>
2023-08-14 14:07:02 -0300

    Aprendendo validar se a URL é válida

---
+++ REAMD.md
@@ -0,0 +1,14 @@
+<h1>Aprendendo Node</h1>
+
+<h2>Tópicos</h2>
+<ul>
+    <li>Módulos Core</li>
+    <li>NPM (Node Package Manager)</li>
+    <li>Módulos de Terceiros</li>
+    <li>Express.js</li>
+    <li>Roteamento</li>
+    <li>Manipulação de Dados</li>
+    <li>Promises e Async/Await</li>
+    <li>Gestão de Erros</li>
+    <li>APIs RESTful</li>
+</ul>
---
+++ index.js
@@ -0,0 +1,28 @@
+// require('alguma função : http, fs ...') é nativo do node
+const http = require('http'); /* Essa função permite criar um server*/
+const fs = require('fs'); /* Essa função com parametro 'fs' permite manipular arquivos*/
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((require, response)=>{
+    //Testando a inicialização do server
+    // response.statusCode = 200;
+    // response.setHeader('content-type','text/plain');
+    // response.end("Olá Alexsandro");
+    
+    if(require.url == '/node'){ /* Aqui foi add um condicional onde só mostrará o conteúdo do arquivo se for igual a /node*/
+        fs.readFile('index.html',(err,data)=>{
+            response.writeHead(200,{'content-type' : 'text/html'});
+            response.write(data);
+            return response.end();
+        })
+    }else{
+        return response.end("Esse diretório não existe!");
+    }
+
+});
+
+// VARIÁVEL + .listen é uma função expecífica do nodejs
+server.listen(port, hostname, ()=>{
+    console.log("Servidor rodando");
+});
